2b85c13a233c1c1ff71a5ad8614d1c2a35239fcd
Hugo Lu <printk@gmail.com>
2016-04-29 17:18:43 +0800

    Monads are Elephants Part 4, sample code v4

---
+++ myNote/monad_io_v4/RTIO.scala
@@ -0,0 +1,34 @@
+//file RTIO.scala
+sealed abstract class IOAction_v4[+A] extends Function1[WorldState, (WorldState, A)] {
+  def map[B](f: A => B): IOAction_v4[B] = flatMap { x => IOAction_v4(f(x)) }
+  def flatMap[B](f: A => IOAction_v4[B]): IOAction_v4[B] = new ChainedAction(this, f)
+
+  private class ChainedAction[+A, B](action1: IOAction_v4[B], f: B => IOAction_v4[A]) extends IOAction_v4[A] {
+    def apply(state1: WorldState) = {
+      val (state2, intermediateResult) = action1(state1)
+      val action2 = f(intermediateResult)
+      action2(state2)
+    }
+  }
+}
+
+object IOAction_v4 {
+  def apply[A](expression: => A): IOAction_v4[A] = new SimpleAction(expression)
+
+  private class SimpleAction[+A](expression: => A) extends IOAction_v4[A] {
+    def apply(state: WorldState) = (state.nextState, expression)
+  }
+}
+
+sealed trait WorldState { def nextState: WorldState }
+
+abstract class IOApplication_v4 {
+  private class WorldStateImpl(id: BigInt) extends WorldState {
+    def nextState = new WorldStateImpl(id + 1)
+  }
+  final def main(args: Array[String]): Unit = {
+    val ioAction = iomain(args)
+    ioAction(new WorldStateImpl(0));
+  }
+  def iomain(args: Array[String]): IOAction_v4[_]
+}
