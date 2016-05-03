c8043764ab0c8452cb6e1915fe69d36157a29526
Hugo Lu <printk@gmail.com>
2016-05-03 11:26:26 +0800

    update the test driver

--- myNote/monads-are-elephants/monad_io_v4/HelloWorld.scala
+++ myNote/monads-are-elephants/monad_io_v4/HelloWorld.scala
@@ -2,5 +2,10 @@
 
 class HelloWorld_v4 extends IOApplication_v4 {
   import RTConsole_v4._
-  def iomain(args: Array[String]) = putString("Hello world")
+  def iomain(args: Array[String]) = for {
+    _ <- putString("This is an example of the IO monad. ")
+    _ <- putString("What's your name? ")
+    name <- getString
+    _ <- putString("Hello " + name)
+  } yield ()
 }
