6a4e9fa7eb0ae0d78d9d15b2bb54065ac379c164
Hugo Lu <printk@gmail.com>
2016-07-03 15:33:38 +0800

    refactory DefaultHandles

--- scala_in_depth/chapter06/Observable/DefaultHandles.scala
+++ scala_in_depth/chapter06/Observable/DefaultHandles.scala
@@ -1,5 +1,5 @@
 trait Defaulthandles extends Observable {
     type Handle = (this.type => Unit)
-    protected def createHandle(callback: this.type => Unit): Handle =
-        callback
+
+    protected def createHandle(callback: this.type => Unit): Handle = callback
 }
