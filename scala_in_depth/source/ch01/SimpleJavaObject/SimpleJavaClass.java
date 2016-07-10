99f7cefa064f7a8a851778d7b830843933be7eed
Hugo Lu <printk@gmail.com>
2016-07-10 11:12:20 +0800

    organize files

---
+++ scala_in_depth/source/ch01/SimpleJavaObject/SimpleJavaClass.java
@@ -0,0 +1,12 @@
+class SimpleJavaClass {
+    private String name;
+    public SimpleJavaClass(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public static SimpleJavaClass create(String name) {
+        return new SimpleJavaClass(name);
+    }
+}
---
+++ scala_in_depth/source/ch01/SimpleJavaObject/test.scala
@@ -0,0 +1,7 @@
+object Test extends App {
+    val x = SimpleJavaClass.create("Test")
+
+    x.getName()
+
+    val y = new SimpleJavaClass("Test")
+}
